710766f70824934126472379b2c7e9406bdbf95e
Akif <kosusakifemre@gmail.com>
2022-12-16 14:31:25 +0300

    Implemented recipe classes partially

--- Database + partial recipe classes + simple gui/db + partial implementation of recipe classes/AdditionActivity.java
+++ Database + partial recipe classes + simple gui/db + partial implementation of recipe classes/AdditionActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -8,33 +9,39 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
-public class DeletionActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class AdditionActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_deletion);
+        setContentView(R.layout.activity_addition);
         IngViewModel viewModel = new ViewModelProvider(this).get(IngViewModel.class);
-        Button delList = findViewById(R.id.button2);
+        Button addList = findViewById(R.id.button2);
         EditText edited = findViewById(R.id.editTextTextPersonName2);
         EditText multi = findViewById(R.id.editTextTextMultiLine);
         multi.setEnabled(false);
-        delList.setOnClickListener(new View.OnClickListener() {
+        EditText number = findViewById(R.id.editTextNumber);
+        addList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String txt = edited.getText().toString();
-                CIngredient c = new CIngredient(txt,1);
-
-                if(!c.name.equals("")){
-                    viewModel.deleteIngredients(c);
-                }
-
+                double sNum = Double.valueOf(number.getText().toString());
+                CIngredient c = new CIngredient(txt,sNum);
+                viewModel.insertIngredient(c);
             }
         });
-        viewModel.getList().observe(this, list1 ->{
+        viewModel.getList().observe(this,list1 ->{
             multi.setText("");
+            Program.currOfUser = new ArrayList<CurrentIngredient>();
             for(CIngredient cIngredient: list1){
                 multi.append(cIngredient.name + " " + cIngredient.num + "\n");
+
+                Program.currOfUser.add(Program.converter(cIngredient));
             }
+            Log.d("Debug",Integer.toString(Program.currOfUser.size()));
+
         });
     }
 }
--- Database + partial recipe classes + simple gui/db + partial implementation of recipe classes/MainActivity.java
+++ Database + partial recipe classes + simple gui/db + partial implementation of recipe classes/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,12 +11,27 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class MainActivity extends AppCompatActivity {
     Button manage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Program.ingredientTypes[0] = new Ingredient("onion",0,0);
+        Program.ingredientTypes[1] = new Ingredient("garlic",0,0);
+        Program.ingredientTypes[2] = new Ingredient("tomatoes",10,8);
+        Program.ingredientTypes[3] = new Ingredient("parsley",0,0);
+        Program.ingredientTypes[4] = new Ingredient("carrot",10,8);
+        Program.ingredientTypes[5] = new Ingredient("potato",10,5);
+        Program.ingredientTypes[6] = new Ingredient("mushroom",1,0);
+        Program.ingredientTypes[7] = new Ingredient("eggplant",10,5);
+        Program.ingredientTypes[8] = new Ingredient("mint",0,0);
+        Program.ingredientTypes[9] = new Ingredient("zucchini",0,0);
+        Program.ingredientTypes[10] = new Ingredient("broccoli",0,0);
+        Log.d("ingList",Program.ingredientTypes[4].toString());
         IngViewModel viewModel = new ViewModelProvider(this).get(IngViewModel.class);
         manage = findViewById(R.id.button);
 
@@ -23,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 switchActivities();
+
             }
         });
 
