29bcf0519b835e07298d6b3d43ea9dc4377344a1
Canberk <canberk1223@gmail.com>
2022-12-18 15:22:57 +0300

    edited java classes

--- GUI/java classes/cookit/IngredientEditActivity.java
+++ GUI/java classes/cookit/IngredientEditActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 
 public class IngredientEditActivity extends AppCompatActivity {
 
-    Button butter, carrot, chicken, cucumber, egg, eggplant, flour, garlic, lemon, meat, milk, mushroom, oliveoil, onion, spagetti, potato, rice, tomatoes;
+    Button butter, carrot, chicken, cucumber, egg, eggplant, flour, garlic, greenpepper, lemon, meat, milk, mushroom, oliveoil, onion,redlentil,redpepper, spagetti, potato, rice, tomatoes,tomatopaste;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +23,20 @@ public class IngredientEditActivity extends AppCompatActivity {
         eggplant = findViewById(R.id.editInventoryEditButton_eggplant);
         flour = findViewById(R.id.editInventoryEditButton_flour);
         garlic = findViewById(R.id.editInventoryEditButton_garlic);
+        greenpepper = findViewById(R.id.editInventoryEditButton_greenpepper);
         lemon = findViewById(R.id.editInventoryEditButton_lemon);
         meat = findViewById(R.id.editInventoryEditButton_meat);
         milk = findViewById(R.id.editInventoryEditButton_milk);
         mushroom = findViewById(R.id.editInventoryEditButton_mushroom);
         oliveoil = findViewById(R.id.editInventoryEditButton_oliveoil);
         onion = findViewById(R.id.editInventoryEditButton_onion);
+        redlentil = findViewById(R.id.editInventoryEditButton_redlentil);
+        redpepper = findViewById(R.id.editInventoryEditButton_redpepper);
         spagetti = findViewById(R.id.editInventoryEditButton_spagetti);
         potato = findViewById(R.id.editInventoryEditButton_potato);
         rice = findViewById(R.id.editInventoryEditButton_rice);
         tomatoes = findViewById(R.id.editInventoryEditButton_tomatoes);
+        tomatopaste =findViewById(R.id.editInventoryEditButton_tomatopaste);
 
 
 
